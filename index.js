40aba495375709d0b6590176a5eedbc7d4863546
Fernando Gomez <fhergomez37@gmail.com>
2017-09-27 16:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ function theBeatlesPlay (musicians, instruments) {
  for (let i = 0,l = musicians.length;i < l;i++){
    array.push(`${musicians[i]} plays ${instruments[i]}`);
  }
-
  return array;
-
 }
 
 function johnLennonFacts (arrayFacts) {
