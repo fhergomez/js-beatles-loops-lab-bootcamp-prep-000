81c615eb50aadd6f153267861f64e48b53e4a007
Fernando Gomez <fhergomez37@gmail.com>
2017-09-27 16:21:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function theBeatlesPlay (musicians, instruments) {
+ var array = [];
+ for (let i = 0,l = musicians.length;i < l;i++){
+   array.push(`${musicians[i]} plays ${instruments[i]}`);
+ }
+
+ return array;
+
+}
+
+function johnLennonFacts (arrayFacts) {
+  var array = [];
+  var i = 0;
+  while (i < arrayFacts.length) {
+    array.push(`${arrayFacts} + !!!`);
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles (number) {
+
+}
